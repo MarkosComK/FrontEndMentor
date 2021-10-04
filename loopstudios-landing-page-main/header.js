20d63692a77dc58ee82dfd196010c153b65cb8c7
Markos Vinicius <markoscomks@gmail.com>
2021-10-04 00:16:21 -0300

    Adição de navbar responsivo

---
+++ loopstudios-landing-page-main/header.js
@@ -0,0 +1,35 @@
+window.addEventListener('scroll', () => {
+    let nav = document.querySelector('.header')
+    let navC = document.querySelector('.h-container1')
+    
+    nav.classList.toggle('scrolling-active', window.scrollY > 0)
+    navC.classList.toggle('scrolling-height', window.scrollY > 0)
+    
+})
+
+let hamburguer = document.querySelector('.hamburguer')
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        hamburguer.classList.remove('none')
+    } else{
+        hamburguer.classList.add('none')
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 940px)")
+  myFunction(x) 
+  x.addListener(myFunction) 
+
+  hamburguer.addEventListener('click', () => {
+      let navLinks = document.querySelector('.links')
+      let nav = document.querySelector('.header')
+      let navC = document.querySelector('.h-container1')
+      
+      hamburguer.classList.toggle('active')
+      navLinks.classList.toggle('links-active')
+      if(window.scrollY == 0){
+          nav.classList.toggle('scrolling-active')
+          navC.classList.toggle('scrolling-height')
+      }
+  })
